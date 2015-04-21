8ebdcd30513351f7ff4a650453bac52c63c55aac
HSklaWe <alexander.schulz10@gmx.de>
2015-04-21 13:50:47 +0200

    Added Impressum and Spielregeln

---
+++ content/Impressum.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">		
+    <meta charset="UTF-8">							
+    <title>Impressum</title>						
+  </head>
+    <body>
+		<div class='impressum'><h1>Impressum</h1>
+		<p>Angaben gem&auml&szlig &sect 5 TMG
+		</p>
+		<p><strong>Hochschule Weserbergland e.V.</strong><br>
+		 Am Stockhof 2<br>
+		 31785 Hameln <br></p> 
+		 
+		<p><strong>Vertreten durch: </strong><br>
+			Rudolf Bandick<br>
+			Petra Pfeiffer<br></p>
+			
+		<p><strong>Registereintrag: </strong><br>
+			Eintragung im Vereinsregister.<br>
+			Registergericht: Hameln<br>
+			Registernummer: VR 100558<br></p>
+			
+		<p><strong>Umsatzsteuer-ID: </strong><br><br>
+		
+			Umsatzsteuer-Identifikationsnummer gem&auml&szlig &sect27a Umsatzsteuergesetz: 22/2F2F215/2F2F03451<br><br></p>
+			
+			<p><strong>Inhaltlich Verantwortliche gem&auml&szlig &sect 6 MDStV:</strong><br>
+			Simon Bruns<br>
+			Philipp Klabunde<br>
+			Benjamin Luhn<br>
+			Jonas P&oumlppelmann<br>
+			Alexander Schulz<br></p>
+			
+		<p><strong><FONT SIZE=4>Haftungsausschluss</FONT></strong><br><br>
+
+		<strong>Haftung f&uumlr Inhalte</strong><br><br>
+		
+		Die Inhalte unserer Seiten wurden mit gr&ouml&szligter Sorgfalt erstellt. F&uumlr die Richtigkeit, Vollst&aumlndigkeit und 
+		Aktualit&aumlt der Inhalte k&oumlnnen wir jedoch keine Gew&aumlhr &uumlbernehmen. Als Diensteanbieter sind wir gem&auml&szlig &sect 7 Abs.1 TMG 
+		f&uumlr eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach &sect&sect 8 bis 10 TMG sind wir als 
+		Diensteanbieter jedoch nicht verpflichtet, &uumlbermittelte oder gespeicherte fremde Informationen zu &uumlberwachen oder nach 
+		Umst&aumlnden zu forschen, die auf eine rechtswidrige T&aumltigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der 
+		Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unber&uumlhrt. Eine diesbez&uumlgliche Haftung ist jedoch 
+		erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung m&oumlglich. Bei Bekanntwerden von entsprechenden 
+		Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.<br>
+		<br>
+		<strong>Haftung f&uumlr Links</strong><br>
+		<br>
+		Unser Angebot enth&aumllt Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb k&oumlnnen wir 
+		f&uumlr diese fremden Inhalte auch keine Gew&aumlhr &uumlbernehmen. F&uumlr die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter 
+		oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf m&oumlgliche Rechtsverst&ouml&szlige 
+		&uumlberpr&uumlft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der 
+		verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen 
+		werden wir derartige Links umgehend entfernen.<br>
+		<br>
+		<strong>Datenschutz</strong><br>
+		<br>
+		Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten m&oumlglich. Soweit auf unseren Seiten personenbezogene 
+		Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit m&oumlglich, stets auf freiwilliger Basis. 
+		Diese Daten werden ohne Ihre ausdr&uumlckliche Zustimmung nicht an Dritte weitergegeben. <br>
+		Wir weisen darauf hin, dass die Daten&uumlbertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitsl&uumlcken aufweisen kann. 
+		Ein l&uumlckenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht m&oumlglich. <br>
+		Der Nutzung von im Rahmen der Impressumspflicht ver&oumlffentlichten Kontaktdaten durch Dritte zur &uumlbersendung von nicht ausdr&uumlcklich 
+		angeforderter Werbung und Informationsmaterialien wird hiermit ausdr&uumlcklich widersprochen. Die Betreiber der Seiten behalten sich 
+		ausdr&uumlcklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.<br></p>
+		</div>
+    </body>
+</html>
---
+++ content/Spielregeln.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">		
+    <meta charset="UTF-8">							
+    <title>Spielregeln</title>						
+  </head>
+    <body>
+  
+        <div class='spielregeln'><h1>Spielregeln</h1>
+
+		<p><strong><FONT SIZE=4>Ziel des Spieles</FONT></strong></p>
+
+		<p>Jeder Spieler muss seine Flotte in Gewässern verstecken und versuchen die
+		gegnerische Flotte vollständig zu versenken. Hierbei ist zu beachten, dass die 
+		Flotte nicht komplett zum Opfer des Feindfeuers werden darf.<br></p>
+
+		<p><strong><FONT SIZE=4>Setzen der Schiffe</FONT></strong></p>
+
+		<p>Die eigene und die gegnerische Flotte besteht aus:<br><br>
+		• 1 Schlachtschiff (5 Treffer nötig zum Versenken)<br>
+		• 2 Kreuzer (4 Treffer nötig zum Versenken)<br>
+		• 3 Fregatten (3 Treffer nötig zum Versenken)<br>
+		• 4 Minensucher (2 Treffer nötig zum Versenken)<br>
+
+
+		Vor dem ersten Zug muss jeder seine Schiffe einsetzen. Die Schiffe müssen so eingesetzt werden, 
+		dass sie sich weder waagerecht noch senkrecht überlappen. Ebenfalls ist ein diagonales setzen nicht erlaubt.
+		Wähle ein Schiff durch Anklicken, drehe es gegebenenfalls durch den entsprechenden Button. Ziehe es auf das Gewässer 
+		und klicke dann auf die gewünschte Position um es abzusetzen. Wenn du ein Schiff wieder entfernen willst, kannst du es 
+		durch Anklicken wieder zurücknehmen. Sind alle Schiffe eingesetzt, bestätige die Aufstellung und das Spiel kann gestartet werden.<br></p>
+
+		<p><strong><FONT SIZE=4>Spielablauf</FONT></strong></p>
+
+		<p>In dem oberen Gewässer ist das Feld des Gegners zu sehen. Hier wird pro Runde ein "Schuss" auf ein beliebiges Feld abgegeben um ein
+		gegnerisches Schiff zu treffen. Das angeklickte Feld wird als bereits "beschossen" markiert. War der Schuss ein Treffer, wird dies
+		angezeigt und ein neues Feld kann beschossen werden. Dies kann auch mehrmals hintereinander geschehen.<br>
+		War der Schuss kein Treffer, so ist der Gegner an der Reihe.<br></p>
+		
+		<p><strong><FONT SIZE=4>Spielende</FONT></strong></p>
+		
+		<p>Gewonnen hat der Spieler, welcher zuerst alle Schiffe der gegnerischen Flotte versenkt hat.</p>
+        </div>
+    </body>
+</html>
