1335bd22a3a3b901e7b871060a8177ba84af7a59
Klabauti <klabunde.philipp@gmx.de>
2015-04-28 13:00:14 +0200

    added function return to html-site

--- Log/einloggen.php
+++ Log/einloggen.php
@@ -2,15 +2,14 @@
 	if ($_SERVER['REQUEST_METHOD'] === "POST") {		
 
 		$regExBenutzername = '/^[A-Za-z0-9]{1,32}$/';		
-		$regExEmail = '/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/';	
-		
+		$regExEmail = '/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/';			
 		$emptyBoolean = false;
 		$regExFehlerBoolean = false;
 		$pwAbgleichFehler = false;
 		$logIn = false;
 
 		if(empty($_POST['benutzername']) || empty($_POST['passwort'])){
-			echo 'Es müssen alle Felder ausgefüllt werden!</br>';
+			echo 'Es muessen alle Felder ausgefuellt werden!</br>';
 			$emptyBoolean = true;
 		}
 		
@@ -20,13 +19,6 @@
 			echo 'Benutzername entspricht nicht den Vorgaben</br>';
 			$regExFehlerBoolean = true;
 		}
-		
-//		if(!$emptyBoolean && (preg_match($regExEmail, $_POST['email']))){
-//			echo 'Email entspricht den Vorgaben</br>'; 
-//		}elseif(!$emptyBoolean){
-//			echo 'Email entspricht nicht den Vorgaben</br>';
-//			$regExFehlerBoolean = true;
-//		}
 		
 		$pwHash = hash('sha256', $_POST['passwort']);
 		  
@@ -38,18 +30,26 @@
 																		
 			$stmt->bindParam(':benutzername', $Benutzername);
 			$Benutzername = $_POST['benutzername'];	
-			$jifjif = $stmt->execute();
-			if($jifjif[0] == $pwHash){
+			$stmt->execute();
+			$jifjif = $stmt->fetchAll(PDO::FETCH_NUM);
+			
+			if($jifjif != NULL && $jifjif[0][0] == $pwHash){
 				echo '<p> Benutzerdaten gefunden! Login!! <p>';
 				$pwAbgleichFehler = true;
+				$logInAttempt = true;
 			}else{
 				echo 'Konto- oder Passworteingabe ist falsch!';
+				$template = file_get_contents("einloggen.html"); // Im Template eine Fehlermeldung schreiben
+				echo $template;
+
 			}
 		}else{
-			echo 'Eingaben nicht vollständig gefüllt oder entspricht nicht den RegEx!';
+			echo 'Eingaben nicht vollstaendig gefuellt oder entspricht nicht den RegEx!';
+			$template = file_get_contents("einloggen.html"); // Im Template eine Fehlermeldung schreiben
+			echo $template;
 		}
 	  
 	}else{
 		echo 'Server nicht erreichbar!';
-	}      
+	} 	
 ?>
