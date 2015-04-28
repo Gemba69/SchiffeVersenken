0278b3e021e97ca6213d83b5508582878151a60d
Klabauti <klabunde.philipp@gmx.de>
2015-04-28 15:34:03 +0200

    update vergleichs- / prüffunktionen für den Login/ registrierung

--- Log/eintragen.php
+++ Log/eintragen.php
@@ -5,9 +5,10 @@
 		$regExEmail = '/^[^0-9][A-z0-9_]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_]+)*[.][A-z]{2,4}$/';	
 		$emptyBoolean = false;
 		$regExFehlerBoolean = false;
-		$pwAbgleichFehler = false;		
+		$pwAbgleichFehler = false;
+	
 
-		if(empty($_POST['benutzername']) || empty($_POST['email']) || empty($_POST['passwort'] || empty($_POST['passwortBestaetigen']))){
+		if(empty($_POST['benutzername']) || empty($_POST['email']) || empty($_POST['passwort']) || empty($_POST['passwortBestaetigen'])){
 			echo 'Es müssen alle Felder ausgefüllt werden!</br>';
 			$emptyBoolean = true;
 		}
@@ -39,28 +40,43 @@
 		if(!$emptyBoolean && !$regExFehlerBoolean && !$pwAbgleichFehler){
 			
 			include "verbinden.php";
-				
-			$stmt = $dbh->prepare("INSERT INTO benutzer (Benutzername, Email, Password) 
-												VALUES (:benutzername, :email, :passwort);");
-												
-			$stmt->bindParam(':benutzername', $Benutzername);
-			$stmt->bindParam(':email', $Email);
-			$stmt->bindParam(':passwort',$Passwort);
-				
+			
+			$stmt1 = $dbh->prepare("SELECT Benutzername FROM benutzer WHERE Benutzername = :benutzername");
+																		
+			$stmt1->bindParam(':benutzername', $Benutzername);
 			$Benutzername = $_POST['benutzername'];
-			$Email = $_POST['email'];
-			$Passwort = $pwHash;
-		
-			if ($stmt->execute()){
-				echo '<p> Erfolgreich registriert! <p>';
+			$stmt1->execute();			
+			$test = $stmt1->fetchAll(PDO::FETCH_NUM);
+			
+			if ($test != NULL){
+				echo '<p> Der Benutzername existiert bereits! <p>';
+			    $template = file_get_contents("eintragen.html"); 
+			    echo $template;
 			}else{
-				print_r($dbh->errorInfo());
+				
+				$stmt2 = $dbh->prepare("INSERT INTO benutzer (Benutzername, Email, Password) 
+													VALUES (:benutzername, :email, :passwort);");
+												
+				$stmt2->bindParam(':benutzername', $Benutzername);
+				$stmt2->bindParam(':email', $Email);
+				$stmt2->bindParam(':passwort',$Passwort);
+				
+				$Benutzername = $_POST['benutzername'];
+				$Email = $_POST['email'];
+				$Passwort = $pwHash;
+		
+				if ($stmt2->execute()){
+					echo '<p> Erfolgreich registriert! <p>';
+				}else{
+					print_r($dbh->errorInfo());
+				}
 			}
 		}else{
 			echo 'Eingaben nicht vollständig gefüllt oder entspricht nicht den RegEx!';
-		}
-	  
+			$template = file_get_contents("eintragen.html"); 
+			echo $template;
+			}	  
 	}else{
 		echo 'Server nicht erreichbar!';
-	}      
+		}	      
 ?>
