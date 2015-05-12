f9511ea1bab7990125c2235ecf33d6da85276abb
Gemba69 <bluhn@web.de>
2015-05-12 13:09:25 +0200

    updated newGame

--- content/newGame.php
+++ content/newGame.php
@@ -3,15 +3,16 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Neues Spiel</title>
+		<script src="newGame.js"></script>
 	</head>
 	<body>
 </br> </br>
 <form>
-<input type="radio" id="KI" name="Player" value="KI">KI</input>
-<input type="radio" id="Real" name="Player" value="REAK>realer Gegner">realer Gegner</input>
+<input type="radio" id="KI" name="Player" value="KI" onclick="checkEnemy()">KI</input>
+<input type="radio" id="Real" name="Player" value="realer Gegner" onclick="checkEnemy()">realer Gegner</input>
 </br> </br>
-Gegnersuche: <input type="text" id="searchfield"></Input>
-<input type="submit" name="Search" value="Suche starten"></input>
+Gegnersuche: <input type="text" id="searchfield" disabled></Input>
+<input type="submit" id="Search"name="Search" value="Suche starten" disabled></input>
 </form>
 
 </br> </br>
@@ -31,7 +32,7 @@ $rank = $query->fetchAll();
 
 ?>
 <form action="test.php" method="POST">
-<table border="1">
+<table id="playerTable" border="1" disabled>
 	<tr>
 		<td>Spieler</td>	
 	</tr>
